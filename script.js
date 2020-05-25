62e181c6723b557be58d2b2932f691ed96d6b60d
Glitch (laser-electric-language) <none>
2020-05-25 03:53:09 +0000

    🚀🌶 Checkpoint ./script.js:3856783/4881

--- script.js
+++ script.js
@@ -1,36 +1,34 @@
-console.log("RUNNING")
+console.log("RUNNING");
 
-const basicPasswordCheck = (password)=> {
+const basicPasswordCheck = password => {
   if (password.length < 10) {
-    return `${password} is too short`
+    return `${password} is too short`;
   } else {
-    return `${password} is valid`
+    return `${password} is valid`;
   }
-}
+};
 
-console.log(basicPasswordCheck("hello")) // Returns "This password is too short"
-console.log(basicPasswordCheck("thisisalongpassword")) // Returns "This is a valid password"
+console.log(basicPasswordCheck("hello")); // Returns "This password is too short"
+console.log(basicPasswordCheck("thisisalongpassword")); // Returns "This is a valid password"
 
-
-const confirmUncommonPassword=(password)=>{
-  password = password.toLowerCase()
-  if (password.includes("abcd1234") || password.includes("password")){
-    return false
+const confirmUncommonPassword = password => {
+  password = password.toLowerCase();
+  if (password.includes("abcd1234") || password.includes("password")) {
+    return false;
   }
   return true;
-}
+};
 
-console.log(confirmUncommonPassword("abcd1234")) // Returns false
-console.log(confirmUncommonPassword("Password123")) // Returns false
-console.log(confirmUncommonPassword("UniqueElephant7")) // Returns true
+console.log(confirmUncommonPassword("abcd1234")); // Returns false
+console.log(confirmUncommonPassword("Password123")); // Returns false
+console.log(confirmUncommonPassword("UniqueElephant7")); // Returns true
 
-const confirmSamePasswords = (password1, password2)=>{
-  return password1==password2
-}
+const confirmSamePasswords = (password1, password2) => {
+  return password1 == password2;
+};
 
-
-console.log(confirmSamePasswords("donut2AreDeliciou3!", "donut2AreDeliciou3!")) // Returns true
-console.log(confirmSamePasswords("Dogs&CatsRGr8", "IlikePeanutButter")) // Returns false
+console.log(confirmSamePasswords("donut2AreDeliciou3!", "donut2AreDeliciou3!")); // Returns true
+console.log(confirmSamePasswords("Dogs&CatsRGr8", "IlikePeanutButter")); // Returns false
 
 /*
 function called checkInvalidCharacters that checks 
@@ -40,29 +38,37 @@ If the string contains one of those characters,
 it should return true. If the string doesn't 
 contain one of those characters, it should return false.
 */
-const checkInvalidCharacters = (password) => {
-  return (password.includes(" ") || password.includes("!") ||
-     password.includes("@") || password.includes("#") ||
-      password.includes("*") || password.includes("(") ||
-      password.includes(")"))
-}
+const checkInvalidCharacters = password => {
+  return (
+    password.includes(" ") ||
+    password.includes("!") ||
+    password.includes("@") ||
+    password.includes("#") ||
+    password.includes("*") ||
+    password.includes("(") ||
+    password.includes(")")
+  );
+};
 
-console.log(checkInvalidCharacters("batman and robin")) // Returns true
-console.log(checkInvalidCharacters("jokerAndPenguin")) // Returns false
+console.log(checkInvalidCharacters("batman and robin")); // Returns true
+console.log(checkInvalidCharacters("jokerAndPenguin")); // Returns false
 
-const requireSpecialCharacters = (password) => {
-  const specials = ["!", "@", "#", "$", "%", "^", "_", " ", "-"]
-  let count = 0
+const requireSpecialCharacters = password => {
+  const specials = "!@#$%^_ -";
+  let count = 0;
   for (const symbol of password) {
-    if (symbol in specials){
-      count += 1
-      console.log(symbol)
-      console.log(count)
+    for (const special of specials) {
+      if (symbol == special) {
+        count += 1;
+        console.log(symbol);
+        console.log(count);
+      }
     }
-  }  
-  return (count >= 2)
-}
+  }
+  console.log(count)
+  return count >= 2;
+};
 
-console.log(requireSpecialCharacters("mighty-Morphin_p0wer Rangers!")) // Returns true
-console.log(requireSpecialCharacters("hereWeG0!")) // Returns false
-console.log(requireSpecialCharacters("ohnoanotherbadguy")) // Returns false
+console.log(requireSpecialCharacters("mighty-Morphin_p0wer Rangers!")); // Returns true
+console.log(requireSpecialCharacters("hereWeG0!")); // Returns false
+console.log(requireSpecialCharacters("ohnoanotherbadguy")); // Returns false
