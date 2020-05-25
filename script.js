e83170bfb6cdb4708848ae2b2efc6bc4152252d0
Glitch (laser-electric-language) <none>
2020-05-25 03:06:10 +0000

    ⛺️🏉 Checkpoint ./script.js:3856783/901

--- script.js
+++ script.js
@@ -13,14 +13,36 @@ console.log(basicPasswordCheck("thisisalongpassword")) // Returns "This is a val
 
 
 const confirmUncommonPassword=(password)=>{
-  if (password.includes()"abcd1234") || password.includes("Password")){
+  password = password.toLowerCase()
+  if (password.includes("abcd1234") || password.includes("password")){
     return false
   }
   return true;
 }
 
+console.log(confirmUncommonPassword("abcd1234")) // Returns false
+console.log(confirmUncommonPassword("Password123")) // Returns false
+console.log(confirmUncommonPassword("UniqueElephant7")) // Returns true
+
+const confirmSamePasswords = (password1, password2)=>{
+  return password1==password2
+}
 
 
-confirmUncommonPassword("abcd1234") // Returns false
-confirmUncommonPassword("Password123") // Returns false
-confirmUncommonPassword("UniqueElephant7") // Returns true
+console.log(confirmSamePasswords("donut2AreDeliciou3!", "donut2AreDeliciou3!")) // Returns true
+console.log(confirmSamePasswords("Dogs&CatsRGr8", "IlikePeanutButter")) // Returns false
+
+/*
+function called checkInvalidCharacters that checks 
+to see if a string contains any of the following 
+invalid characters: (a space), !, @, #, or $. 
+If the string contains one of those characters, 
+it should return true. If the string doesn't 
+contain one of those characters, it should return false.
+*/
+const checkInvalidCharacters = (password) => {
+  if (password.co)
+}
+
+console.log(checkInvalidCharacters("batman and robin")) // Returns true
+console.log(checkInvalidCharacters("jokerAndPenguin")) // Returns false
