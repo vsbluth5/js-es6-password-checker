13d56a00a29c8d6da602e20b6b11131646153889
Glitch (laser-electric-language) <none>
2020-05-25 02:55:08 +0000

    🍟👂 Checkpoint ./script.js:3856783/389 ./index.html:3856783/727

--- script.js
+++ script.js
@@ -1,13 +1,26 @@
 console.log("RUNNING")
 
 const basicPasswordCheck = (password)=> {
-  if (password.length <= 5) {
+  if (password.length < 10) {
     return `${password} is too short`
   } else {
-    return `${password} is too short```
+    return `${password} is valid`
   }
 }
 
-
 console.log(basicPasswordCheck("hello")) // Returns "This password is too short"
-basicPasswordCheck("thisisalongpassword") // Returns "This is a valid password"
+console.log(basicPasswordCheck("thisisalongpassword")) // Returns "This is a valid password"
+
+
+const confirmUncommonPassword=(password)=>{
+  if (password.includes()"abcd1234") || password.includes("Password")){
+    return false
+  }
+  return true;
+}
+
+
+
+confirmUncommonPassword("abcd1234") // Returns false
+confirmUncommonPassword("Password123") // Returns false
+confirmUncommonPassword("UniqueElephant7") // Returns true
